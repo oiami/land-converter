5fcf0aa7c868f1bae8134d9ffb7620b3f16b6b82
Pattawan Kaewduangdee <pattawanna@gmail.com>
2019-02-22 22:10:08 +0100

    create result components to display result

---
+++ src/components/Result.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ResultDisplay = props => {
+  return (
+    <div className="ui message"><h2>{props.results.amount} {props.results.unit}</h2></div>
+  );
+};
+
+export default ResultDisplay;
